a1f50f4b1b78b2eb39dbb6d734db4056fa981cff
BIgFudge <87513995+BigFudge420@users.noreply.github.com>
2023-07-08 13:36:14 +0530

    Create function to display all items inside myLibrary
    Used forEach to iterate through all items in myLibrary, and display all the values of each item as a seperate card

--- index.js
+++ index.js
@@ -14,4 +14,46 @@ function addBookToLibrary(book){
     myLibrary.push(book)
 }
 
+addBookToLibrary(book2)
+addBookToLibrary(book1)
+console.log(myLibrary)
+
+const Library = document.querySelector('.library')
+function displayBook(){
+    myLibrary.forEach((book) => {
+        let Title = book.titleOfTheBook
+        let Author = book.authorName
+        let Pages = book.numberOfPages
+        let Status = book.readStatus
+        
+        const cardElement = document.createElement('div')
+        cardElement.classList.add('card')
+        Library.appendChild(cardElement)
+
+        const titleElement = document.createElement('p')
+        titleElement.classList.add('card-item')
+        titleElement.classList.add('title')
+        titleElement.textContent = Title
+        cardElement.appendChild(titleElement)
+
+        const authorElement = document.createElement('p')
+        authorElement.classList.add('card-item')
+        authorElement.classList.add('author')
+        authorElement.textContent = Author
+        cardElement.appendChild(authorElement)
+
+        const pagesElement = document.createElement('p')
+        pagesElement.classList.add('card-item')
+        pagesElement.classList.add('pages')
+        pagesElement.textContent = Pages
+        cardElement.appendChild(pagesElement)
+
+        const statusElement = document.createElement('p')
+        statusElement.classList.add('card-item')
+        statusElement.classList.add('status')
+        statusElement.textContent = Status
+        cardElement.appendChild(statusElement)
+    })
+}
+
 
